4a3aca2b4391cbd6139337a08a1ee3ccd9d76d4d
Samuel Roach <samuelroach.2000@gmail.com>
2020-02-13 19:45:44 +0000

    finished file reader

--- streaming/src/core/DataHandler.java
+++ streaming/src/core/DataHandler.java
@@ -1,32 +1,71 @@
 package core;
 
-import model.Video;
+import model.*;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DataHandler {
     static ArrayList<Video> allVideos = new ArrayList<Video>();
+    static int requestDesc;
+    static DataCenter dataCenter;
 
-    public static void readData() {
+    public static DataCenter readData() {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("./src/me_at_the_zoo.in"));
+            Scanner reader = new Scanner("./src/me_at_the_zoo.in");
 
-            reader.readLine();
-            String secondLine = reader.readLine();
-            String[] videoSizesStr = secondLine.split(" ");
+            String[] firstLine = reader.nextLine().split(" ");
+            requestDesc = Integer.parseInt(firstLine[2]);
+            int cacheCount = Integer.parseInt(firstLine[3]);
+            int cacheSize = Integer.parseInt(firstLine[4]);
 
-            for (int i = 0; i < videoSizesStr.length; i ++) {
-                allVideos.add(new Video(Integer.parseInt(videoSizesStr[i])));
+            for (int i = 0; i <cacheCount; i ++) {
+                dataCenter.addCache(new Cache(cacheSize));
             }
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            String secondLine = reader.nextLine();
+            String[] videoSizesStr = secondLine.split(" ");
+
+            for (int i = 0; i < videoSizesStr.length; i++) {
+                dataCenter.addVideo(new Video(Integer.parseInt(videoSizesStr[i])));
+            }
+
+            while (reader.hasNext()) {
+                String[] currentLine = reader.nextLine().split(" ");
+                if (currentLine.length < 3) {
+                    int dcLatency = Integer.parseInt(currentLine[0]);
+                    int epCount = Integer.parseInt(currentLine[1]);
+
+                    Endpoint temporaryEP = new Endpoint(dcLatency);
+
+                    for (int i = 0; i < epCount; i ++) {
+                        String[] latencyToCache = reader.nextLine().split(" ");
+                        temporaryEP.addCache(dataCenter.getCaches().get(Integer.parseInt(latencyToCache[0])), Integer.parseInt(latencyToCache[1]));
+                    }
+
+                    dataCenter.addEndpoint(temporaryEP);
+
+                } else {
+                    String[] requestsLine = reader.nextLine().split(" ");
+                    int videoNumber = Integer.parseInt(requestsLine[0]);
+                    Video video = dataCenter.getVideos().get(videoNumber);
+                    int epPos = Integer.parseInt(requestsLine[1]);
+                    int amount = Integer.parseInt(requestsLine[2]);
+                    Endpoint requestingEP = dataCenter.getEndpoints().get(epPos);
+
+                    dataCenter.getEndpoints().get(epPos).addRequest(new Request(video, requestingEP, amount));
+                }
+
+            }
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
+        return dataCenter;
     }
 }

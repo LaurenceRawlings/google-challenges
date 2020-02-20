f3f7c27f20f8374e09a7f10985b25b6eb38f016a
Laurence Rawlings <rawlingslaurence@gmail.com>
2020-02-20 17:39:53 +0000

    Update

--- qualifier/reader.py
+++ qualifier/reader.py
@@ -5,7 +5,32 @@ class Reader:
     
     def __call__(self):
         with open(self.path, 'r') as file:
-            dict = {}
+            lines = file.readlines()
+            dict = {
+                'videos': [],
+                'endpoints': [],
+                'requests': [],
+                'caches': [],
+                'cache_max': 0
+            }
+
+            info = lines[0].split(' ')
+            videos = lines[1].split(' ')
+
+            for i in range(0, len(videos)):
+                dict.get('videos').append((i, int(videos[i])))
+
+            line = 2
+            endpoints = {}
+            for i in range(0, int(info[1])):
+                endpoint = lines[line].split(' ')
+                for j in range(0, endpoint[1]):
+                    line += 1
+
+            
+
+
+            
 
 
 

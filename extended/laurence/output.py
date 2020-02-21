87f78e9e64d9d4addeee2b30fac03aebe5f192c8
Laurence Rawlings <rawlingslaurence@gmail.com>
2020-02-21 01:02:28 +0000

    Create folder for my extended attempt

---
+++ extended/laurence/output.py
@@ -0,0 +1,27 @@
+class Output:
+
+    def __init__(self, path):
+        self.path = path
+    
+    def __call__(self, libraries):
+        f = open(self.path, 'w')
+
+        outputText = str(len(libraries)) + '\n'
+
+        for library in libraries :
+            bookQueue = library.get('book_queue')
+            queueLength = len(bookQueue)
+            currentBook = 0
+
+            if not (queueLength == 0):
+                outputText = outputText + str(library.get('id')) + ' ' + str(queueLength) + '\n'
+            
+            for book in bookQueue :
+                if currentBook < queueLength - 1 :
+                    outputText = outputText + book + ' '
+                    currentBook += 1
+                else :
+                    outputText = outputText + book + '\n'
+
+        f.write(outputText)
+        f.close
---
+++ extended/laurence/reader.py
@@ -0,0 +1,32 @@
+class Reader:
+
+    def __init__(self, path):
+        self.path = path
+    
+    def __call__(self):
+        with open(self.path, 'r') as file:
+            lines = file.read().split('\n')
+            dict = {
+                'book_scores': [],
+                'libraries': [],
+                'days': 0
+            }
+
+            info = lines[0].split(' ')
+            dict.update( {'book_scores' : lines[1].split(' ')} )
+            dict.update( {'days' : info[2]} )
+
+            line = 2
+            for i in range(0, int(info[1])):
+                library = lines[line].split(' ')
+                line += 1
+                dict.get('libraries').append( {
+                    'id': i, 
+                    'signup': library[1], 
+                    'scanrate': library[2], 
+                    'books': lines[line].split(' '),
+                    'book_queue' : []
+                    } )
+                line += 1
+
+        return dict

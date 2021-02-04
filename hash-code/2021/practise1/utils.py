0ac73a7de045eb1453adae9db431fc06ab4227cc
Laurence Rawlings <rawlingslaurence@gmail.com>
2021-02-04 19:07:21 +0000

    Reader

--- hash-code/2021/practise1/utils.py
+++ hash-code/2021/practise1/utils.py
@@ -15,3 +15,21 @@ def insert_sorted_dict_array(array: list, item: dict, key: str):
 
     return array[:half] + [item] + array[half:]
 
+
+def read(file_name: str):
+    with open(file_name, 'r') as file:
+        lines = file.read().split('\n')
+
+    photos = []
+    for i in range(1, int(lines[0]) + 1):
+        line = lines[i].split(' ')
+        is_horizontal = line[0] == 'H'
+        tag_count = int(line[1])
+        tags = set(line[2:])
+        photos.append({
+            'is_horizontal': is_horizontal,
+            'tag_count': tag_count,
+            'tags': tags
+        })
+
+    return photos
